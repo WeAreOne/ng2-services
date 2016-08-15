3ad2dd6cd0fc75c188b0c305aaf41a03922dd5f4
Frédéric Dupérier <fduperier@gmail.com>
2016-08-15 21:50:45 +0200

    retour sur les providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { AppComponent }   from './app.component';
 
 import { ViewportService } from "./services/ViewportService";
 import { SubComponent } from "./sub";
-import { CustomService } from "./services/CustomService";
-import { Title } from "@angular/platform-browser";
 
 @NgModule({
   declarations: [ AppComponent, SubComponent ],
@@ -15,9 +13,7 @@ import { Title } from "@angular/platform-browser";
     { provide: 'SizeService',
       useFactory: (viewport: any) => viewport.determineService(),
       deps: [ ViewportService ]
-    },
-    { provide: 'SizeService', useClass: CustomService },
-    Title
+    }
   ]
 })
 export class AppModule {}
--- src/app/sub/sub.component.ts
+++ src/app/sub/sub.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { Title } from "@angular/platform-browser";
-import { SizeService } from "../services";
+import { CustomService, SizeService } from "../services";
 
 @Component({
   moduleId: module.id,
   selector: 'app-sub',
   templateUrl: 'sub.component.html',
-  styleUrls: ['sub.component.css']
+  styleUrls: ['sub.component.css'],
+  providers: [ { provide: 'SizeService', useClass: CustomService } ],
+  viewProviders: [ Title ]
 })
 export class SubComponent implements OnInit {
 
